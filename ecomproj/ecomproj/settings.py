83b8ac69cd5b617b8967205e4ac44ffaaf1f159a
YuanhuiXu <xuuuyuanhui@foxmail.com>
2024-12-12 11:26:31 +0000

    feature: admin setup

--- ecomproj/ecomproj/settings.py
+++ ecomproj/ecomproj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -133,3 +134,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Configure media files
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    'site_header': "Once Read",
+    'site_brand': "Once Read",
+    # 'site_logo': "static/assets/",
+    # 'site_copyright'
+    }
+    
