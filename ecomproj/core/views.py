7144d21ed378b54a3e3fd5f88924d89a69f0ec64
YuanhuiXu <xuuuyuanhui@foxmail.com>
2024-12-18 16:44:39 +0000

    feat: display products

--- ecomproj/core/views.py
+++ ecomproj/core/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, Review, Favorites, Address
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    products = Product.objects.all()
+
+    context = {
+        "products": products
+    }
+
+    return render(request, 'core/index.html', context)
